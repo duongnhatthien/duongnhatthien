693f858b4992a4c4e613f8dd31f0bb87326c60ee
ThienCodeWeb <duongnhatthien888@gmail.com>
2022-03-22 23:18:22 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Nguyễn Huỳnh Như <33",
+    introTitle: 'Hellu chị !!',
+    introDesc: `Giờ em quá nhớ chị ròy..phải làm seo..phải làm seo :>`,
+    btnIntro: '^-^ Hihi ^-^',
+    title: 'Chị cũng nhớ em đúng khummm  🥰',
+    desc: '',
+    btnYes: 'Nhớ lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại nhớ emmm <3',
+    btnReply: 'Gửi cho Thiên <3',
+    reply: 'Vì chị lỡ thích em ròy <3',
+    mess: 'Emm biết mà 🥰. Êuu chụy nhiều nhiều 😘😘',
+    messDesc: '',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/iwillcoder.203/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
